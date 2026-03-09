e0e60b9347ba353e4e3fb9cc8fe4c8eb7fcbfc3f
Jason Lin <jasonlin01052010@gmail.com>
2026-03-09 08:27:54 -0400

    Update lab_1a.py
    Added my name

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -5,10 +5,11 @@ The first lab in the BWSI CSS course. To complete this lab, fill out the variabl
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
 
+
 def main():
     print("Hello World!")
 
-    name = "" # TODO: Insert your name between the double quotes
+    name = "Jason Lin"  # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
 
